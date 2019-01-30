2556d4eb03498945b76773d5091758400b59343c
Nemanja Jovic <nemanja.jovic195@gmail.com>
2019-01-30 12:46:14 +0100

    initial commit

---
+++ header.php
@@ -0,0 +1,36 @@
+<?php 
+    session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <title>Login System</title>
+</head>
+<body>
+    <header>
+        <nav class="nav-header-main">
+            <a href="index.php" class="header-logo">
+                <img class="logo" src="logo.png" alt="Logo">
+            </a>
+            <div class="header-login">
+                <?php 
+                    if (isset($_SESSION['userId'])) {
+                        echo '<form action="includes/logout.inc.php" method="post">
+                        <button class="log-out" type="submit" name="logout-submit">Log out</button>
+                    </form>';
+                    } else {
+                        echo '<form action="includes/login.inc.php" method="post">
+                        <input type="text" name="mailuid" placeholder="Username/e-mail">
+                        <input type="password" name="pwd" placeholder="Password">
+                        <button type="submit" name="login-submit">Log in</button>
+                    </form>
+                    <a class="sign-up" href="signup.php">Sign up</a>';
+                    }
+                ?> 
+            </div>
+        </nav>
+    </header>
